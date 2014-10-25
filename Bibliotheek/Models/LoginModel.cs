6c661bed5816b2f65aa8f7bf7f20d21855e8a20a
Miesvanderlippe <miesvanderlippe@gmail.com>
2014-10-26 00:01:21 +0200

    Added account/login controllers/views/models
    And some smaller changes

---
+++ Bibliotheek/Models/LoginModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibliotheek.Models
+{
+    public class LoginModel
+    {
+        [Display(Name = "Email: ")]
+        [Required(ErrorMessage = "Email is verplicht")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Wachtwoord is verplicht")]
+        [Display(Name = "Wachtwoord:")]
+        public string Password { get; set; }
+    }
+}
